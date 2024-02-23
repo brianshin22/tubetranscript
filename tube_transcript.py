48dd1af6b7bbe51edcaac73ad55f1bfb9afec4d5
Brian Shin <brianshin22@hotmail.com>
2024-02-22 21:55:11 -0800

    use openai whisper api instead of locally

--- tube_transcript.py
+++ tube_transcript.py
@@ -1,42 +1,56 @@
-import streamlit as st
-import whisper
 import os
-# from openai import OpenAI
+
+import streamlit as st
+# import whisper
 from pytube import YouTube
+
+from openai import OpenAI
+
+from helper import format_transcript
+
+system_prompt = "You are a helpful assistant. Your task is to add necessary punctuation such as periods, commas, and capitalization, and use only the context provided."
+
+def generate_corrected_transcript(temperature, system_prompt, raw_transcript):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        temperature=temperature,
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": raw_transcript
+            }
+        ]
+    )
+    return response.choices[0].message.content
+
+# openai_api_key = osenviron.get('OPENAI_API_KEY')
 
 if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
     st.session_state.placeholder = "Enter Youtube URL"
 
-# video_folder = "/home/brian/projects/whisper/"
-yt = None
-
 st.title("Youtube Video Transcriber")
 st.caption("A tool by Brian Shin. [brianshin.me](https://brianshin.me)")
 
-# st.sidebar.header("Translation Section")
 st.divider()
-#audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
 
-
-# process_button = st.sidebar.button("Translate audio")
-# st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
 with st.container():
     col1, col2 = st.columns([6,2])
     with col2:
         process_button = st.button("Transcribe video")
     with col1: 
         url = st.text_input("Enter Youtube URL below", "", label_visibility="collapsed", placeholder="Enter Youtube URL")
-    # with col3:
-        # translate_button = st.button("Translate video")
 
 
 st.divider()
 
-
-
-model = whisper.load_model("base")
+# model = whisper.load_model("base")
+client = OpenAI()
 
 if process_button:
     st.header("Video")
@@ -46,7 +60,6 @@ if process_button:
     # st.header("Transcription")
     transcript_container = st.container()
     transcript_container.header("Transcript")
-    status_placeholder = transcript_container.empty()
     # client = OpenAI()
     # task = "Translate the following from Korean to English: "
     transcript_container.empty()
@@ -63,27 +76,39 @@ if process_button:
         video_container.error("Please choose a video shorter than 10 minutes.")
         st.stop()
     video_container.video(url)
-    status_placeholder.info("Transcribing video...")
-    stream = yt.streams.get_by_itag(140)
-    # audio_file = stream.download(video_folder)
-    audio_file = stream.download()
-    print(audio_file)
-    if audio_file is not None:
-        #filepath = "/home/brian/Downloads/" + audio_file.name
-        filepath = audio_file
-        result = model.transcribe(filepath)
+    with st.spinner("Transcribing video..."):
+        stream = yt.streams.get_by_itag(140)
+        # audio_file = stream.download(video_folder)
+        audio_file = stream.download()
+        print(audio_file)
+        if audio_file is not None:
+            #filepath = "/home/brian/Downloads/" + audio_file.name
+            filepath = audio_file
+            file = open(audio_file, "rb")
+            # result = model.transcribe(filepath)
+            result = client.audio.transcriptions.create(
+                model="whisper-1",
+                file=file,
+                response_format="text"
+            )
 
-        transcript = result['text']
-        from helper import format_transcript
-        formatted_transcript = format_transcript(transcript)
-        
-        print(formatted_transcript)
+            # transcript = result['text']
+            transcript = result
+            print("Original transcript: " + transcript)
+            transcript_punctuated = generate_corrected_transcript(0, system_prompt, transcript)
+            print("Punctuated transcript: " + transcript_punctuated)
+            # format with newlines for easier viewing
+            # formatted_transcript = format_transcript(transcript)
+            formatted_transcript2 = format_transcript(transcript_punctuated)
+            # print(formatted_transcript)
+            # print(formatted_transcript2)
 
-        transcript_container.markdown(formatted_transcript)
-        status_placeholder.empty()
-        status_placeholder.success("Finished transcribing video")
-    else:
-        st.error("Please enter a valid URL")
+            transcript_container.markdown(formatted_transcript2)
+
+
+
+    # else:
+    #     st.error("Please enter a valid URL")
 
 
 # if translate_button:
